c1a7b1f9f137322039749c70e06f334a94c1a7fe
Eduardo Pinho <enet4mikeenet@gmail.com>
2018-01-28 17:24:03 +0000

    Adjust travis CI (one for the entire project)

--- faiss-sys/travis/install_faiss_c.sh
+++ faiss-sys/travis/install_faiss_c.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 repo_url=https://github.com/Enet4/faiss.git
-repo_rev=78bd95d45bb87d33dc47bfd5d27353635cdfcca2
+repo_rev=ae982e22b7688c40b2d5ba3d1cc07935c0020fce
 
 git clone $repo_url faiss
 cd faiss
@@ -14,7 +14,9 @@ make libfaiss_c.so
 mkdir -p $HOME/.faiss_c
 cp libfaiss_c.so $HOME/.faiss_c/
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.faiss_c/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.faiss_c
+
+echo libfaiss_c.so installed in $HOME/.faiss_c/
 
 cd ..
 
