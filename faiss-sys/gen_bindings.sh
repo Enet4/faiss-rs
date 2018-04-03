b009270d7898ec89cf74c5e5c069a42e32b69a42
Eduardo Pinho <enet4mikeenet@gmail.com>
2018-04-03 15:39:50 +0100

    [sys] Update faiss repo revisions

--- faiss-sys/gen_bindings.sh
+++ faiss-sys/gen_bindings.sh
@@ -6,10 +6,10 @@ if ! which bindgen > /dev/null; then
 fi
 
 repo_url=https://github.com/Enet4/faiss.git
-repo_rev=4d440b6698fcc7b08607534bc622902b52bf9c49
+repo_rev=3bdc5abeaf0cea28430b34fd02b5f9491c122884
 cuda_root=/opt/cuda
 
-git clone $repo_url faiss
+git clone --single-branch $repo_url faiss
 cd faiss
 git checkout -q $repo_rev
 cd ..
--- faiss-sys/travis/install_faiss_c.sh
+++ faiss-sys/travis/install_faiss_c.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 repo_url=https://github.com/Enet4/faiss.git
-repo_rev=4d440b6698fcc7b08607534bc622902b52bf9c49
+repo_rev=3bdc5abeaf0cea28430b34fd02b5f9491c122884
 
-git clone $repo_url faiss
+git clone --single-branch $repo_url faiss
 cd faiss
 git checkout -q $repo_rev
 
