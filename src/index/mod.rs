67bca21a56dc29d7db8ac95fae4521df1b853551
Justin <austin@kensho.com>
2018-12-01 03:26:12 +0000

    fix typo

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -36,7 +36,7 @@ pub type Idx = idx_t;
 /// Although all methods appear to be available for all index implementations,
 /// some methods may not be supported. For instance, a [`FlatIndex`] stores
 /// vectors sequentially, and so does not support `add_with_ids` nor
-/// `remove_with_ids`. Users are advised to read the Faiss wiki pages in order
+/// `remove_ids`. Users are advised to read the Faiss wiki pages in order
 /// to understand which index algorithms support which operations.
 /// 
 /// [`FlatIndex`]: flat/struct.FlatIndex.html
