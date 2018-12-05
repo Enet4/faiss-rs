c533de89b78641bfb3aa6dca64c095f19c50f91d
Justin <austin@kensho.com>
2018-12-05 23:21:58 +0000

    added beginning of selector mod

---
+++ src/selector.rs
@@ -0,0 +1,24 @@
+//! Abstract Faiss ID selector
+use faiss_sys::*;
+
+/// Abstraction over IDSelectorRange and IDSelectorBatch
+#[derive(Debug)]
+pub struct IdSelector {
+    inner: *mut FaissIDSelector,
+}
+
+impl IdSelector {
+    /// Create new range selector
+    pub fn range(min: idx_t, max: idx_t) -> IdSelector {
+        let mut sel = FaissIDSelector_H {_unused: []};
+        let _ = faiss_IDSelectorRange_new(&mut &mut sel, min, max);
+        IdSelector { inner: &mut sel }
+    }
+
+    /// Create new batch selector
+    pub fn batch(n: i64, indices: &idx_t) -> IdSelector {
+        let mut sel = FaissIDSelector_H {_unused: []};
+        let _ = faiss_IDSelectorBatch_new(&mut &mut sel, n, indices);
+        IdSelector { inner: &mut sel }
+    }
+}
