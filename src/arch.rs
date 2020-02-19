daeb2a094be53c8da15bc5c443e48bef853ba407
Danil Guskov <guskovd86@mail.ru>
2020-02-19 12:57:09 +0300

    arm support

--- src/arch.rs
+++ src/arch.rs
@@ -1,5 +1,21 @@
-#[cfg(any(target_arch = "aarch64", target_arch = "x86_64"))]
+#[cfg(
+    any(
+	target_arch = "x86_64",
+	target_arch = "aarch64",
+	target_arch = "powerpc64",
+	target_arch = "mips64",
+	target_arch = "nvptx"
+    )
+)]
 pub type faiss_usize = i64;
 
-#[cfg(target_arch = "x86")]
+#[cfg(
+    any(
+	target_arch = "x86",
+	target_arch = "arm",
+	target_arch = "powerpc",
+	target_arch = "mips",
+	target_arch = "wasm32"
+    )
+)]
 pub type faiss_usize = i32;
