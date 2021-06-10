4ec80d20ef9cb650d28e9b6eb286e419f448842f
Eduardo Pinho <enet4mikeenet@gmail.com>
2021-06-10 10:55:05 +0100

    Tweak CI
    - add caching of Faiss content
    - copy Faiss .so files to system lib path
      instead of updating LD_LIBRARY_PATH
       - the other way did not work

--- faiss-sys/ci/install_faiss_c.sh
+++ faiss-sys/ci/install_faiss_c.sh
@@ -6,21 +6,33 @@ repo_rev=c_api_head
 
 git clone $repo_url faiss --branch $repo_rev --depth 1
 
+mkdir -p "$HOME/.faiss_c"
+
 cd faiss
+
+git rev-parse HEAD > ../rev_hash
+
+if [[ -s "$HOME/.faiss_c/rev_hash" && `diff -w -q ../rev_hash $HOME/.faiss_c/rev_hash` -eq "0" ]]; then
+    echo "libfaiss_c.so is already built for revision" `cat ../rev_hash`
+
+    # clean up
+    cd ..
+    rm -rf faiss rev_hash
+    exit 0
+fi
+
 
 # Build
 cmake . -DFAISS_ENABLE_C_API=ON -DBUILD_SHARED_LIBS=ON \
-    -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_C_FLAGS_DEBUG="-g -O1" -DCMAKE_CXX_FLAGS_DEBUG="-g -O1" \
+    -DCMAKE_BUILD_TYPE=Release \
     -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF
 
 make
-mkdir -p "$HOME/.faiss_c"
-cp faiss/libfaiss.so c_api/libfaiss_c.so "$HOME/.faiss_c/"
+cp -f "../rev_hash" faiss/libfaiss.so c_api/libfaiss_c.so "$HOME/.faiss_c/"
 
-echo "libfaiss_c.so installed in $HOME/.faiss_c/"
+echo "libfaiss_c.so (" `cat ../rev_hash` ") installed in $HOME/.faiss_c/"
 
 cd ..
 
 # clean up
-rm -rf faiss
+rm -rf faiss rev_hash
