e78a162ccc9a93756da9b6292240aead63fce44c
Alexander Andreev <andreevlex.as@gmail.com>
2021-06-13 21:18:50 +0300

    Fix ffi ownership mistakes

--- src/index/refine_flat.rs
+++ src/index/refine_flat.rs
@@ -4,25 +4,27 @@ use super::*;
 
 use crate::error::Result;
 use crate::faiss_try;
+use std::marker::PhantomData;
 use std::mem;
 use std::os::raw::c_int;
 use std::ptr;
 
 /// Alias for the native implementation of a index.
-pub type RefineFlatIndex = RefineFlatIndexImpl;
+pub type RefineFlatIndex<BI> = RefineFlatIndexImpl<BI>;
 
 /// Native implementation of a RefineFlat index.
 #[derive(Debug)]
-pub struct RefineFlatIndexImpl {
+pub struct RefineFlatIndexImpl<BI> {
     inner: *mut FaissIndexRefineFlat,
+    base_index: PhantomData<BI>,
 }
 
-unsafe impl Send for RefineFlatIndexImpl {}
-unsafe impl Sync for RefineFlatIndexImpl {}
+unsafe impl<BI: Send> Send for RefineFlatIndexImpl<BI> {}
+unsafe impl<BI: Sync> Sync for RefineFlatIndexImpl<BI> {}
 
-impl CpuIndex for RefineFlatIndexImpl {}
+impl<BI: CpuIndex> CpuIndex for RefineFlatIndexImpl<BI> {}
 
-impl Drop for RefineFlatIndexImpl {
+impl<BI> Drop for RefineFlatIndexImpl<BI> {
     fn drop(&mut self) {
         unsafe {
             faiss_IndexRefineFlat_free(self.inner);
@@ -30,15 +32,11 @@ impl Drop for RefineFlatIndexImpl {
     }
 }
 
-impl RefineFlatIndexImpl {
-    pub fn new<I: NativeIndex>(base_index: I) -> Result<Self> {
+impl<BI: NativeIndex> RefineFlatIndexImpl<BI> {
+    pub fn new(base_index: BI) -> Result<Self> {
         let index = RefineFlatIndexImpl::new_helper(&base_index, true)?;
         mem::forget(base_index);
         Ok(index)
-    }
-
-    pub fn new_by_ref<I: NativeIndex>(base_index: &I) -> Result<Self> {
-        RefineFlatIndexImpl::new_helper(base_index, false)
     }
 
     fn new_helper<I: NativeIndex>(base_index: &I, own_fields: bool) -> Result<Self> {
@@ -49,7 +47,10 @@ impl RefineFlatIndexImpl {
                 base_index.inner_ptr(),
             ))?;
             faiss_IndexRefineFlat_set_own_fields(inner, c_int::from(own_fields));
-            Ok(RefineFlatIndexImpl { inner })
+            Ok(RefineFlatIndexImpl {
+                inner,
+                base_index: PhantomData,
+            })
         }
     }
 
@@ -64,25 +65,161 @@ impl RefineFlatIndexImpl {
     }
 }
 
-impl NativeIndex for RefineFlatIndexImpl {
+impl<BI> NativeIndex for RefineFlatIndexImpl<BI> {
     fn inner_ptr(&self) -> *mut FaissIndex {
         self.inner
     }
 }
 
-impl FromInnerPtr for RefineFlatIndexImpl {
+impl<BI> FromInnerPtr for RefineFlatIndexImpl<BI> {
     unsafe fn from_inner_ptr(inner_ptr: *mut FaissIndex) -> Self {
         RefineFlatIndexImpl {
             inner: inner_ptr as *mut FaissIndexFlat,
+            base_index: PhantomData,
         }
     }
 }
 
-impl_native_index!(RefineFlatIndexImpl);
+impl<BI> Index for RefineFlatIndexImpl<BI> {
+    fn is_trained(&self) -> bool {
+        unsafe { faiss_Index_is_trained(self.inner_ptr()) != 0 }
+    }
 
-impl_native_index_clone!(RefineFlatIndexImpl);
+    fn ntotal(&self) -> u64 {
+        unsafe { faiss_Index_ntotal(self.inner_ptr()) as u64 }
+    }
 
-impl ConcurrentIndex for RefineFlatIndexImpl {
+    fn d(&self) -> u32 {
+        unsafe { faiss_Index_d(self.inner_ptr()) as u32 }
+    }
+
+    fn metric_type(&self) -> MetricType {
+        unsafe { MetricType::from_code(faiss_Index_metric_type(self.inner_ptr()) as u32).unwrap() }
+    }
+
+    fn add(&mut self, x: &[f32]) -> Result<()> {
+        unsafe {
+            let n = x.len() / self.d() as usize;
+            faiss_try(faiss_Index_add(self.inner_ptr(), n as i64, x.as_ptr()))?;
+            Ok(())
+        }
+    }
+
+    fn add_with_ids(&mut self, x: &[f32], xids: &[Idx]) -> Result<()> {
+        unsafe {
+            let n = x.len() / self.d() as usize;
+            faiss_try(faiss_Index_add_with_ids(
+                self.inner_ptr(),
+                n as i64,
+                x.as_ptr(),
+                xids.as_ptr() as *const _,
+            ))?;
+            Ok(())
+        }
+    }
+    fn train(&mut self, x: &[f32]) -> Result<()> {
+        unsafe {
+            let n = x.len() / self.d() as usize;
+            faiss_try(faiss_Index_train(self.inner_ptr(), n as i64, x.as_ptr()))?;
+            Ok(())
+        }
+    }
+    fn assign(&mut self, query: &[f32], k: usize) -> Result<AssignSearchResult> {
+        unsafe {
+            let nq = query.len() / self.d() as usize;
+            let mut out_labels = vec![Idx::none(); k * nq];
+            faiss_try(faiss_Index_assign(
+                self.inner_ptr(),
+                nq as idx_t,
+                query.as_ptr(),
+                out_labels.as_mut_ptr() as *mut _,
+                k as i64,
+            ))?;
+            Ok(AssignSearchResult { labels: out_labels })
+        }
+    }
+    fn search(&mut self, query: &[f32], k: usize) -> Result<SearchResult> {
+        unsafe {
+            let nq = query.len() / self.d() as usize;
+            let mut distances = vec![0_f32; k * nq];
+            let mut labels = vec![Idx::none(); k * nq];
+            faiss_try(faiss_Index_search(
+                self.inner_ptr(),
+                nq as idx_t,
+                query.as_ptr(),
+                k as idx_t,
+                distances.as_mut_ptr(),
+                labels.as_mut_ptr() as *mut _,
+            ))?;
+            Ok(SearchResult { distances, labels })
+        }
+    }
+    fn range_search(&mut self, query: &[f32], radius: f32) -> Result<RangeSearchResult> {
+        unsafe {
+            let nq = (query.len() / self.d() as usize) as idx_t;
+            let mut p_res: *mut FaissRangeSearchResult = ::std::ptr::null_mut();
+            faiss_try(faiss_RangeSearchResult_new(&mut p_res, nq))?;
+            faiss_try(faiss_Index_range_search(
+                self.inner_ptr(),
+                nq,
+                query.as_ptr(),
+                radius,
+                p_res,
+            ))?;
+            Ok(RangeSearchResult { inner: p_res })
+        }
+    }
+
+    fn reset(&mut self) -> Result<()> {
+        unsafe {
+            faiss_try(faiss_Index_reset(self.inner_ptr()))?;
+            Ok(())
+        }
+    }
+
+    fn remove_ids(&mut self, sel: &IdSelector) -> Result<usize> {
+        unsafe {
+            let mut n_removed = 0;
+            faiss_try(faiss_Index_remove_ids(
+                self.inner_ptr(),
+                sel.inner_ptr(),
+                &mut n_removed,
+            ))?;
+            Ok(n_removed)
+        }
+    }
+
+    fn verbose(&self) -> bool {
+        unsafe { faiss_Index_verbose(self.inner) != 0 }
+    }
+
+    fn set_verbose(&mut self, value: bool) {
+        unsafe {
+            faiss_Index_set_verbose(self.inner, std::os::raw::c_int::from(value));
+        }
+    }
+}
+
+impl<BI> RefineFlatIndexImpl<BI> {
+    /// Create an independent clone of this index.
+    ///
+    /// # Errors
+    ///
+    /// May result in a native error if the clone operation is not
+    /// supported for the internal type of index.
+    pub fn try_clone(&self) -> Result<Self> {
+        unsafe {
+            let mut new_index_ptr = ::std::ptr::null_mut();
+            faiss_try(faiss_clone_index(self.inner_ptr(), &mut new_index_ptr))?;
+            Ok(crate::index::FromInnerPtr::from_inner_ptr(new_index_ptr))
+        }
+    }
+}
+
+impl<BI> ConcurrentIndex for RefineFlatIndexImpl<BI>
+where
+    BI: ConcurrentIndex,
+{
     fn assign(&self, query: &[f32], k: usize) -> Result<AssignSearchResult> {
         unsafe {
             let nq = query.len() / self.d() as usize;
@@ -177,49 +314,5 @@ mod tests {
 
         refine.reset().unwrap();
         assert_eq!(refine.ntotal(), 0);
-    }
-
-    #[test]
-    fn refine_flat_index_ref_search() {
-        let index = FlatIndexImpl::new_l2(D).unwrap();
-        assert_eq!(index.d(), D);
-        assert_eq!(index.ntotal(), 0);
-
-        let mut refine = RefineFlatIndexImpl::new_by_ref(&index).unwrap();
-        let some_data = &[
-            7.5_f32, -7.5, 7.5, -7.5, 7.5, 7.5, 7.5, 7.5, -1., 1., 1., 1., 1., 1., 1., -1., 0., 0.,
-            0., 1., 1., 0., 0., -1., 100., 100., 100., 100., -100., 100., 100., 100., 120., 100.,
-            100., 105., -100., 100., 100., 105.,
-        ];
-        refine.add(some_data).unwrap();
-        assert_eq!(refine.ntotal(), 5);
-        assert_eq!(index.ntotal(), 5);
-
-        let my_query = [0.; D as usize];
-        let result = refine.search(&my_query, 5).unwrap();
-        assert_eq!(
-            result.labels,
-            vec![2, 1, 0, 3, 4]
-                .into_iter()
-                .map(Idx::new)
-                .collect::<Vec<_>>()
-        );
-        assert!(result.distances.iter().all(|x| *x > 0.));
-
-        let my_query = [100.; D as usize];
-        // flat index can be used behind an immutable ref
-        let result = (&refine).search(&my_query, 5).unwrap();
-        assert_eq!(
-            result.labels,
-            vec![3, 4, 0, 1, 2]
-                .into_iter()
-                .map(Idx::new)
-                .collect::<Vec<_>>()
-        );
-        assert!(result.distances.iter().all(|x| *x > 0.));
-
-        refine.reset().unwrap();
-        assert_eq!(refine.ntotal(), 0);
-        assert_eq!(index.ntotal(), 0);
     }
 }
