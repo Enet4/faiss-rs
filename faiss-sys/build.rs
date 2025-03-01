55c120dd9cfff975dd288aa61a1594d788e8586d
Ibiyemi Abiodun <ibiyemi@intulon.com>
2025-02-28 19:55:24 -0500

    add oneapi support

--- faiss-sys/build.rs
+++ faiss-sys/build.rs
@@ -7,6 +7,8 @@ fn main() {
 
 #[cfg(feature = "static")]
 fn static_link_faiss() {
+    use std::path::PathBuf;
+
     let mut cfg = cmake::Config::new("faiss");
 
     cfg.define("FAISS_ENABLE_C_API", "ON")
@@ -42,10 +44,66 @@ fn static_link_faiss() {
     );
     println!("cargo:rustc-link-lib=static=faiss_c");
     println!("cargo:rustc-link-lib=static=faiss");
+
+    if let Ok(oneapi_root) = std::env::var("ONEAPI_ROOT") {
+        println!("using ONEAPI_ROOT={oneapi_root:?}");
+
+        // if using Intel oneAPI (such as on Windows), we need libraries from
+        // the mkl folder and the compiler folder
+        let oneapi_root = PathBuf::from(oneapi_root);
+        let mkl_root = oneapi_root.join("mkl").join("latest");
+        let compiler_root = oneapi_root.join("compiler").join("latest");
+
+        println!(
+            "cargo:rustc-link-search=native={}",
+            mkl_root.join("bin").display()
+        );
+        println!(
+            "cargo:rustc-link-search={}",
+            mkl_root.join("lib").display()
+        );
+
+        println!(
+            "cargo:rustc-link-search=native={}",
+            compiler_root.join("bin").display()
+        );
+        println!(
+            "cargo:rustc-link-search={}",
+            compiler_root.join("lib").display()
+        );
+
+        println!("cargo:rustc-link-lib=mkl_intel_lp64_dll");
+        println!("cargo:rustc-link-lib=mkl_intel_thread_dll");
+        println!("cargo:rustc-link-lib=mkl_core_dll");
+        println!("cargo:rustc-link-lib=libiomp5md");
+    } else if let Ok(mkl_root) = std::env::var("MKLROOT") {
+        println!("using MKLROOT={mkl_root:?}");
+        let mkl_root = PathBuf::from(mkl_root);
+
+        println!(
+            "cargo:rustc-link-search=native={}",
+            mkl_root.join("bin").display()
+        );
+        println!(
+            "cargo:rustc-link-search=native={}",
+            mkl_root.join("lib").display()
+        );
+
+        println!("cargo:rustc-link-lib=dylib=mkl_core_dll");
+        println!("cargo:rustc-link-lib=dylib=mkl_intel_lp64_dll");
+        println!("cargo:rustc-link-lib=dylib=mkl_intel_thread_dll");
+        println!("cargo:rustc-link-lib=dylib=libiomp5md");
+    } else {
+        println!("cargo:rustc-link-lib=gomp");
+        println!("cargo:rustc-link-lib=blas");
+        println!("cargo:rustc-link-lib=lapack");
+    }
+
+    println!("cargo:rerun-if-env-changed=ONEAPI_ROOT");
+    println!("cargo:rerun-if-env-changed=MKLROOT");
+
     link_cxx();
-    println!("cargo:rustc-link-lib=gomp");
-    println!("cargo:rustc-link-lib=blas");
-    println!("cargo:rustc-link-lib=lapack");
+
     if cfg!(feature = "gpu") {
         let cuda_path = cuda_lib_path();
         println!("cargo:rustc-link-search=native={}/lib64", cuda_path);
